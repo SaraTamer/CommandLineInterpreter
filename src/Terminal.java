29fed861280c94e385cf6e7591cef95f34f8f04d
GannaAhmed22 <143264752+gannaAhmad@users.noreply.github.com>
2023-10-27 23:56:02 +0200

    cp ,wc and pwd implementation

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.Paths;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -38,8 +41,55 @@ public class Terminal {
         }
         return exists;
     }
-    public String pwd(){return " ";}
+    public String pwd(){return System.getProperty("user.dir");}
     public void cd(String[] args){}
+    public boolean cp(String[] arg) {
+        String source = arg[0];
+        String dest = arg[1];
+        String check=".txt";
+        boolean flag = source.contains(check);
+        if(!flag){
+            source+=".txt";
+        }
+        try (FileInputStream sourceFileInputStream = new FileInputStream(source);
+             FileOutputStream destinationFileOutputStream = new FileOutputStream(dest)) {
+
+            int bytesRead;
+            while ((bytesRead = sourceFileInputStream.read()) != -1) {
+                destinationFileOutputStream.write(bytesRead);
+            }
+
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean wc(String[] arg) throws Exception {
+        try {
+            Path file = Paths.get(arg[0]);
+            long count_l = 0;
+            long count_c = 0;
+            long count_w = 0;
+            String line;
+            BufferedReader reader = Files.newBufferedReader(file);
+            while ((line = reader.readLine()) != null) {
+                count_l++;
+                count_c += line.length();
+                String[] words = line.split("\\s+");
+                count_w += words.length;
+            }
+            System.out.println("Total Lines: " + count_l);
+            System.out.println("Total Words: " + count_w);
+            System.out.println("Total Chars: " + count_c);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+
     public void chooseCommandAction(String input)
     {
         parser.parse(input);
@@ -53,6 +103,19 @@ public class Terminal {
                 for (String exist : exists)
                     System.out.println("mkdir: can't create directory '" + exist + "': File exists");
                 break;
+            case "wc":
+                try {
+                    wc(parser.getArgs());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "pwd":
+                System.out.println(pwd());
+                break;
+            case "cp":
+                cp(parser.getArgs());
+                break;
             case "exit":
                 System.exit(0);
                 break;
@@ -62,8 +125,7 @@ public class Terminal {
 
         }
     }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Terminal myterminal = new Terminal();
         Scanner in = new Scanner(System.in);
         while (true)
